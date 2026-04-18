269c4cfa9c0ef284b4214ddde874e228ce203eb8
Rohit Paul <rohit.paul1705@gmail.com>
2026-04-18 20:52:30 +0530

    Change API_URL to production backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const API_URL = "http://localhost:5000";
+export const API_URL = "https://airbnb-clone-backend-sum8.onrender.com";
 const api = axios.create({ baseURL: `${API_URL}/api` });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
